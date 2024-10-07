aca86cbdc577c32c1789f0cdae15ba48e020ef5c
jal <85102600+jalsmida321@users.noreply.github.com>
2024-10-07 11:48:03 +0800

    Update GamePage.tsx

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -14,7 +14,7 @@ const GamePage: React.FC = () => {
     <div className="max-w-7xl mx-auto">
       <h1 className="text-4xl font-bold text-center text-gray-900 mb-8">{decodeURIComponent(gameName || '')}</h1>
       <div id="game" class="ratio ratio-16x9 overflow-hidden rounded-3 mb-4 game-ratio" style="display:none;">
-   <iframe id="ubgs" class="game-iframe lazyload" src="about:blank"  src={gameUrl}
+   <iframe src={gameUrl}
           title={gameName} width="850" height="590" scrolling="none" frameborder="0" allowfullscreen></iframe>
       </div>
     </div>

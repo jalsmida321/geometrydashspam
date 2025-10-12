837e9c1a3d5357f650950087dd558ba706b4488e
jal <85102600+jalsmida321@users.noreply.github.com>
2025-10-12 23:18:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
+        // 明确列出所有游戏页面，确保没有遗漏
         'aka-geometry-dash-spam': './games/aka-geometry-dash-spam.html',
-        'geometry-dash-spam-wave': './games/geometry-dash-spam-wave.html',
-        'geometry-dash-spam-test': './games/Geometry-Dash-Spam-Test.html',
+        'Geometry-Dash-Spam-Test': './games/Geometry-Dash-Spam-Test.html',
         'geometry-dash-spam-challenge': './games/geometry-dash-spam-challenge.html',
         'geometry-dash-spam-chall': './games/geometry-dash-spam-chall.html',
         'geometry-dash-spam-master': './games/geometry-dash-spam-master.html',
+        'geometry-dash-spam-wave': './games/geometry-dash-spam-wave.html',
         'geometry-dash-wave-spam': './games/geometry-dash-wave-spam.html'
       }
     }

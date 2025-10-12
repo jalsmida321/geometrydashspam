8c355a9a35e1e1483d0e4ec95138faeb5242a94c
jal <85102600+jalsmida321@users.noreply.github.com>
2025-10-12 22:53:24 +0800

    修复部署问题

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        'aka-geometry-dash-spam': './games/aka-geometry-dash-spam.html',
+        'geometry-dash-spam-wave': './games/geometry-dash-spam-wave.html',
+        'geometry-dash-spam-test': './games/Geometry-Dash-Spam-Test.html'
+      }
+    }
   },
   server: {
     port: 3000
